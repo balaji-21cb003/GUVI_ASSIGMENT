99b03278245fa223546123263ea382b37e2cef68
balaji-21cb003 <balaji.s021csbs@sece.ac.in>
2024-03-22 14:42:49 +0530

    guvi task

--- php/update.php
+++ php/update.php
@@ -2,12 +2,20 @@
 require_once 'vendor/autoload.php'; 
 
 use MongoDB\Client as MongoDBClient;
+use Predis\Client as PredisClient;
 
 // Connect to MongoDB
 $databaseConnection = new MongoDBClient; 
 $myDatabase = $databaseConnection->mydatabase;
 
 $userCollection = $myDatabase->users;
+
+// Create a Predis client instance
+$predisClient = new PredisClient([
+    'scheme' => 'tcp',
+    'host' => '127.0.0.1',
+    'port' => 6379,
+]);
 
 // Retrieve form data
 $id = $_POST['id'] ?? '';
@@ -34,6 +42,16 @@ $updateResult = $userCollection->updateOne(['_id' => new MongoDB\BSON\ObjectID($
 // Check if update was successful
 if ($updateResult->getModifiedCount() > 0) {
     echo "Profile updated successfully";
+    
+    // Store user data in Redis
+    $userData = [
+        'name' => $name,
+        'email' => $email,
+        'dob' => $dob,
+        'phone' => $phone,
+        'address' => $address,
+    ];
+    $predisClient->hmset("user:$id", $userData);
 } else {
     echo "Failed to update profile";
 }
